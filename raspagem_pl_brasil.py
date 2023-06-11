2b096779a47f87641bb0af664583eafd8669bef2
Natália Santos <tanatelha.dados@gmail.com>
2023-06-11 19:30:55 -0300

    commit dois

--- raspagem_pl_brasil.py
+++ raspagem_pl_brasil.py
@@ -1,68 +1,76 @@
-import os
-from dotenv import load_dotenv
+# bibliotecas que já vem com python
+import os #biblioteca para ver chaves em ambiente virtual
 
+
+# bibliotecas externas: import em ordem alfabética e depois froms em ordem alfabética
 import gspread
-import json
-import re 
 import requests
-from datetime import datetime
+from flask import Flask
 from bs4 import BeautifulSoup
+from datetime import time, timedelta, datetime
 from oauth2client.service_account import ServiceAccountCredentials 
 
+
 # importanto funcoes de outros arquivos do repositório
-from data import data_de_hoje
 from contagem_palavras import contagem_palavras
 
-# Carrega as variáveis de ambiente do arquivo .env
-load_dotenv()
 
 # variáveis de ambiente
-GOOGLE_SHEETS_KEY = os.environ.get('GOOGLE_SHEETS_KEY')
 
-GOOGLE_SHEETS_CREDENTIALS = os.environ.get('GOOGLE_SHEETS_CREDENTIALS')
-with open("credenciais.json", mode="w") as arquivo:
-    arquivo.write(GOOGLE_SHEETS_CREDENTIALS)
-conta = ServiceAccountCredentials.from_json_keyfile_name("credenciais.json")
+GOOGLE_SHEETS_KEY = os.environ["GOOGLE_SHEETS_KEY"] 
+GOOGLE_SHEETS_CREDENTIALS = os.environ['GOOGLE_SHEETS_CREDENTIALS']
 
-
+conta = ServiceAccountCredentials.from_json_keyfile_name(GOOGLE_SHEETS_CREDENTIALS)
 api = gspread.authorize(conta)
 planilha = api.open_by_key(f'{GOOGLE_SHEETS_KEY}') 
-sheet_raspagens = planilha.worksheet('raspagem')
+
+sheet_raspagem = planilha.worksheet('raspagem')
 
 
+# Criação do site
+app = Flask(__name__)
 
+
+@app.route("/")
+def index():
+  return "Esse é um site."
+
+
+@app.route("/raspagem-pl")
 def raspagem_pl_brasil():
-    
-    raspagem = []
-    data_hoje_funcao = data_de_hoje()
-    
-    # sites que serão raspados
-    url_pl_brasil = ['https://premierleaguebrasil.com.br/category/futebol-ingles/',
-                     'https://premierleaguebrasil.com.br/category/futebol-europeu/']
-    
-    for url in url_pl_brasil:
-        resposta = requests.get(f'{url}')
-        site_pl = BeautifulSoup(resposta.content)
+  raspagem = []
 
-        noticias = site_pl.findAll('article')
+  data_de_ontem = data_ontem()
 
-        for i in noticias:
-            data_hora = i.find('time', {'class' :'ct-meta-element-date' }).text
-            data_noticia = data_hora[0:5]
+  # sites que serão raspados
+  url_pl_brasil = ['https://premierleaguebrasil.com.br/category/futebol-ingles/',
+                  'https://premierleaguebrasil.com.br/category/futebol-europeu/']
 
-            if data_noticia == data_hoje_funcao:
-                manchete = (i.find('h2', {'class' : 'entry-title'}).text).replace('\n', '')
-                autor = i.find('span', {'itemprop':'name'}).text
-                data = data_hora[0:5] # selecionar as primeiras letras
-                hora = data_hora[11:] # selecionar as últimas letras
-                link = i.find('a').get('href')
+  for url in url_pl_brasil:
+    resposta = requests.get(f'{url}')
+    site_pl = BeautifulSoup(resposta.content)
 
-                palavras = contagem_palavras(url)
-        
-        raspagem.append(["", manchete, "", autor, str(data_hora), hora, "", palavras, link])
-    
-    sheet_raspagens.append_rows(raspagem)
+    noticias = site_pl.findAll('article')
 
-    return f'{raspagem}'
+    for i in noticias:
+      data_hora = i.find('time', {'class' :'ct-meta-element-date' }).text
+      data_noticia = data_hora[0:10]
+      
+      if data_noticia != data_de_ontem:
+        continue
 
-print(raspagem_pl_brasil())
+      else:  
+        texto = i
+        manchete = (texto.find('h2', {'class' : 'entry-title'}).text).replace('\n', '')
+        autor = texto.find('span', {'itemprop':'name'}).text
+        data = data_hora[0:5] # selecionar as primeiras letras
+        hora = data_hora[11:16] # selecionar as últimas letras
+        link = texto.find('a').get('href')
+
+        palavras = contagem_palavras(link)
+
+      raspagem.append(["Category", manchete, "Angle/Comment", autor, str(data), hora, "Publish article", palavras, link])
+
+  sheet_raspagem.append_rows(raspagem)
+
+  return f'Foram adicionados {len(raspagem)} matérias'
